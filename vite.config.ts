fbd24c15a9ffb9afb031365a614015fefbf6ec7d
Le Sang <3lesang@gmail.com>
2024-11-21 22:12:45 +0700

    init commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,13 @@
 import react from "@vitejs/plugin-react";
-import path from "path";
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "src"),
     },
   },
 });
