7071c09d36dfb4214d4589bca58f192f6af964ea
Le Sang <3lesang@gmail.com>
2024-11-21 23:06:08 +0700

    chore(router): set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
 export default function App() {
-  return <p>app</p>;
+  return <RouterProvider router={router} />;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [TanStackRouterVite({ routesDirectory: "src/pages" }), react()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
