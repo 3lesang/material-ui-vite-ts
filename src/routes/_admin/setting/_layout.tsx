c217e5edb9edc5319c09206d1e0511c4812c69bb
Sang Le <sang.le@dision.tech>
2024-12-30 17:40:24 +0700

    fix(ui): update

--- src/routes/_admin/setting/_layout.tsx
+++ src/routes/_admin/setting/_layout.tsx
@@ -1,8 +1,12 @@
+import { SettingsOutlined } from "@mui/icons-material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
 import Grid2 from "@mui/material/Grid2";
-import MenuItem from "@mui/material/MenuItem";
-import MenuList from "@mui/material/MenuList";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 import { createFileRoute, Link, Outlet } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_admin/setting/_layout")({
@@ -14,15 +18,27 @@ function RouteComponent() {
     <Grid2 container spacing={1}>
       <Grid2 size={3}>
         <Card>
-          <CardHeader title="Setting" />
-          <MenuList>
-            <MenuItem component={Link} to="/setting/user">
-              User
-            </MenuItem>
-            <MenuItem component={Link} to="/setting/role">
-              Role
-            </MenuItem>
-          </MenuList>
+          <List dense disablePadding>
+            <ListItem>
+              <ListItemText>Setting</ListItemText>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton dense component={Link} to="/setting/user">
+                <ListItemIcon>
+                  <AccountCircleIcon />
+                </ListItemIcon>
+                <ListItemText>User</ListItemText>
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component={Link} to="/setting/role">
+                <ListItemIcon>
+                  <SettingsOutlined />
+                </ListItemIcon>
+                <ListItemText>Role</ListItemText>
+              </ListItemButton>
+            </ListItem>
+          </List>
         </Card>
       </Grid2>
       <Grid2 size={9}>
