2d8fb3872e94057c6cd51d9583ed2aeaaca816f0
Sang Le <3lesang@gmail.com>
2025-02-14 18:24:09 +0700

    fix(env): update

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.BASE_URL,
+  baseURL: import.meta.env.VITE_BASE_URL,
 });
 
 const onRequest = (config: InternalAxiosRequestConfig) => {
