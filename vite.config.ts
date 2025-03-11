9557eb417b0ddec1354305537346ba53dca3d13f
Sang Le <sang.le@dision.tech>
2025-03-11 13:12:15 +0700

    feat(pwa): add

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
-
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [
+    TanStackRouterVite(),
+    react(),
+    VitePWA({ registerType: "autoUpdate" }),
+  ],
   server: {
     port: 5500,
     open: true,
