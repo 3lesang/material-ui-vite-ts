dfff7043e19ac2b9410e193ac274941065290206
Sang Le <sang.le@dision.tech>
2025-03-24 16:54:57 +0700

    fix(editor): update

--- src/components/ui/CustomEditor.tsx
+++ src/components/ui/CustomEditor.tsx
@@ -1,33 +1,7 @@
-import StarterKit from "@tiptap/starter-kit";
-import {
-  MenuButtonBold,
-  MenuButtonItalic,
-  MenuControlsContainer,
-  MenuDivider,
-  MenuSelectHeading,
-  RichTextEditor,
-  type RichTextEditorRef,
-} from "mui-tiptap";
-import { useRef } from "react";
+import { Editor } from "@dugong.tech/editor";
 
 function AppEditor() {
-  const rteRef = useRef<RichTextEditorRef>(null);
-
-  return (
-    <RichTextEditor
-      ref={rteRef}
-      extensions={[StarterKit]}
-      content="<p>Hello world</p>"
-      renderControls={() => (
-        <MenuControlsContainer>
-          <MenuSelectHeading />
-          <MenuDivider />
-          <MenuButtonBold />
-          <MenuButtonItalic />
-        </MenuControlsContainer>
-      )}
-    />
-  );
+  return <Editor />;
 }
 
 export default AppEditor;
